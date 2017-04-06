ea2e4540e5a4c7160c7e1fb81ccc410a6e0a708d
philcapon <phil.capon@fastmail.co.uk>
2017-04-06 07:35:37 +0100

    problems 39-41

---
+++ project-euler/src/test/pjc/projecteuler/Problem039Test.java
@@ -0,0 +1,18 @@
+package pjc.projecteuler;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Project Euler Problem
+ * Created by philc_000 on 23/03/2017.
+ */
+
+public class Problem039Test {
+
+    @Test
+    public void testSingleCall() {
+        Problem039 prob = new Problem039(20);
+    }
+}
