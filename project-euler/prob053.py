7c561498fb6a1e7eec733300a2d6b97de82922d9
philc511 <phil.capon@fastmail.co.uk>
2019-07-13 19:10:01 +0100

    solution for 53  (brute force)

--- project-euler/prob053.py
+++ project-euler/prob053.py
@@ -1,13 +1,11 @@
 def ncr(n, r):
     total = 1
-    for i in range(1,2):
-        print(i)
-        total = total * (n+1-r)
-        total = total // i
+    for i in range(r):
+        total = total * (n-i)
+        total = total // (i+1)
     return total
-x = range(6)
-for n in x:
-  print(n) 
-print (ncr(5,2))
-#print (ncr(5,3))
-#print (ncr(23,11))
+count = 0
+for i in range(1,101):
+    for j in range(1,i):
+        if(ncr(i, j) > 1000000): count +=1
+print (count)
