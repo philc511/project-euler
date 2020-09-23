7fca1190f18b9e19337c488d7fe841c90c1330dc
philc511 <phil.capon@fastmail.co.uk>
2020-09-23 20:27:44 +0100

    change params in 058

--- project-euler/spiral_primes_058.py
+++ project-euler/spiral_primes_058.py
@@ -1,8 +1,8 @@
 import Euler
-primes = Euler.sieveOfErasthenes(400000000)
+primes = Euler.sieveOfErasthenes(536870911)
 prime_count = 0
 
-for i in range(1,10000):
+for i in range(1,11580):
   leading_diag = (2*i+1)*(2*i+1)
   if (primes[leading_diag-2*i] == 1):
     prime_count+=1
