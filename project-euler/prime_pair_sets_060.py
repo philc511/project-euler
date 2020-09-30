93b365b3fd52744adee94bba0a80418da24e2493
philc511 <phil.capon@fastmail.co.uk>
2020-09-30 22:35:35 +0100

    new approach

--- project-euler/prime_pair_sets_060.py
+++ project-euler/prime_pair_sets_060.py
@@ -2,39 +2,21 @@ import itertools
 import time
 import Euler
 
-def pairs(elements):
-    res = []
-    head, *tail = elements
-    if len(elements) == 2:
-        res.append((head, tail[0]))
-    else:
-        for elt in tail:
-            res.append((head, elt))
-        res+=pairs(tail)
-    return res
-
+# observation - a pair of primes with different modulo 3 values will not be prime pair (apart from those including 3 itself)
 
 def is_concat_pair_prime(pair, sieve):
   return Euler.is_prime(int(str(pair[0]) + str(pair[1])), sieve) and Euler.is_prime(int(str(pair[1]) + str(pair[0])), sieve)
   
-def look_for_pairs(primes, prime_pairs, set_size):
-    max_sum = 0
-    for set in itertools.combinations(primes,set_size):
-        is_all_prime = True
-        for pair in itertools.combinations(set,2):
-            if not prime_pairs[pair]:
-                is_all_prime = False
-                break
-        if is_all_prime:
-            print(set)
-            set_sum = sum(set)
-            if set_sum > max_sum:
-                max_sum = set_sum
-                print(max_sum)
-    return max_sum
 
-PRIME_MAX=10000
-SET_SIZE=4
+def look_for_pairs_of_pairs(pairs, sieve):
+    for set in itertools.combinations(pairs, 2):
+        if is_concat_pair_prime((set[0][0], set[1][0]), sieve) \
+            and is_concat_pair_prime((set[0][0], set[1][1]), sieve) \
+            and is_concat_pair_prime((set[0][1], set[1][0]), sieve) \
+            and is_concat_pair_prime((set[0][1], set[1][1]), sieve):
+            print(set)
+
+PRIME_MAX=5000
 sieve = Euler.sieveOfErasthenes(100000000)
 primes=[]
 for i in range(3,PRIME_MAX):
@@ -43,26 +25,26 @@ for i in range(3,PRIME_MAX):
 primes.remove(5)
 print("done sieve")
 
-prime_pairs_list = []
-prime_pairs={}
-for i in primes:
-    for j in primes: 
-        prime_pairs[(i,j)] = is_concat_pair_prime((i,j), sieve)
-        prime_pairs[(j,i)] = prime_pairs[(i,j)]
-        if is_concat_pair_prime((i,j), sieve) and i < j:
-            prime_pairs_list.append((i,j))
-print(len(prime_pairs_list))
+prime_pairs_list_rem_1 = []
+prime_pairs_list_rem_2 = []
+for pair in itertools.combinations(primes, 2):
+    if pair[0] % 3 + pair[1] % 3 == 0:
+        # can't be prime
+        break
+    non_three = max(pair)
+    if is_concat_pair_prime(pair, sieve):
+        if non_three%3 == 1:
+            prime_pairs_list_rem_1.append(pair)
+        else:
+            prime_pairs_list_rem_2.append(pair)
+
 print("done creating array of pairs")
 
 start =time.perf_counter()
-p_rem_1=[3]
-p_rem_2=[3]
-for i in primes:
-    if i % 3 == 1:
-        p_rem_1.append(i)
-    if i % 3 == 2:
-        p_rem_2.append(i)
-print(look_for_pairs(p_rem_1, prime_pairs, SET_SIZE))
-print(look_for_pairs(p_rem_2, prime_pairs, SET_SIZE))
+# these will look for pairs of pairs (including duplicates, took 16 secs with PRIME_MAX=5000 on macbook)
+#look_for_pairs_of_pairs(prime_pairs_list_rem_1, sieve)
+#look_for_pairs_of_pairs(prime_pairs_list_rem_2, sieve)
+# alt approach - add one more prime to the tuple, then another one
+
 end= time.perf_counter()
 print(end - start)
