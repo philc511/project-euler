c184cb3c857ed3b10a807bc8285499687714403e
philc511 <phil.capon@fastmail.co.uk>
2020-10-02 08:23:34 +0100

    solution to problem 60

--- project-euler/prime_pair_sets_060.py
+++ project-euler/prime_pair_sets_060.py
@@ -3,49 +3,17 @@ import time
 import Euler
 
 # observation - a pair of primes with different modulo 3 values will not be prime pair (apart from those including 3 itself)
+# so separate out primes into two lists,  p mod 3 = 1 and p mod 3 = 2 (3 itself in both lists
+# Then for each of these start with list of primes, then check all primes above the 
+# max of these to see if it can be added to make a valid pair.
+# Repeat on each pair to make a tripe, quadruplet then finally again to make five.
+# Took about a week of playing with this, after trying brute force and other approaches which were all too slow
 
+# function to check if a pair is prime, ie concatenating the numbers in either order gives a prime number
 def is_concat_pair_prime(pair, sieve):
   return Euler.is_prime(int(str(pair[0]) + str(pair[1])), sieve) and Euler.is_prime(int(str(pair[1]) + str(pair[0])), sieve)
   
-
-def look_for_pairs_of_pairs(pairs, sieve):
-    for set in itertools.combinations(pairs, 2):
-        if is_concat_pair_prime((set[0][0], set[1][0]), sieve) \
-            and is_concat_pair_prime((set[0][0], set[1][1]), sieve) \
-            and is_concat_pair_prime((set[0][1], set[1][0]), sieve) \
-            and is_concat_pair_prime((set[0][1], set[1][1]), sieve):
-            print(set)
-
-PRIME_MAX=5000
-sieve = Euler.sieveOfErasthenes(50000000)
-primes=[]
-for i in range(3,PRIME_MAX):
-    if sieve[i] == 1:
-        primes.append(i)
-primes.remove(5)
-print("done sieve")
-
-prime_pairs_list_rem_1 = []
-prime_pairs_list_rem_2 = []
-for pair in itertools.combinations(primes, 2):
-    if pair[0] % 3 + pair[1] % 3 == 0:
-        # can't be prime
-        break
-    non_three = max(pair)
-    if is_concat_pair_prime(pair, sieve):
-        if non_three%3 == 1:
-            prime_pairs_list_rem_1.append(pair)
-        else:
-            prime_pairs_list_rem_2.append(pair)
-
-print("done creating array of pairs")
-
-start =time.perf_counter()
-# these will look for pairs of pairs (including duplicates, took 16 secs with PRIME_MAX=5000 on macbook)
-#look_for_pairs_of_pairs(prime_pairs_list_rem_1, sieve)
-#look_for_pairs_of_pairs(prime_pairs_list_rem_2, sieve)
-# alt approach - add one more prime to the tuple, then another one
-
+# take a list of sets (n-tuples) and find primes which can be added to that tuple to make a list of n+1-tuples (for primes where p mod 3 = req_remainder
 def expand_prime_sets(prime_sets_in, primes, sieve, req_remainder):
     prime_sets_out = []
     for p in prime_sets_in:
@@ -61,17 +29,35 @@ def expand_prime_sets(prime_sets_in, primes, sieve, req_remainder):
                     prime_sets_out.append(p + (i,))
     return prime_sets_out
 
-def generate_sets_with_remainder(req_remainder, primes, sieve):
-    init_primes=[(3,)]
+# starting from a list of primes (p mod 3 = req_remainder), iteratively create a list of
+# size-tuples, by making pairs from the primes then triples from those, etc
+def generate_sets_with_remainder(size, req_remainder, primes, sieve):
+    res=[(3,)]
     for i in primes:
         if i%3 == req_remainder:
-            init_primes.append((i,))
-    pairs = expand_prime_sets(init_primes, primes, sieve, req_remainder)
-    triples = expand_prime_sets(pairs, primes, sieve, req_remainder)
-    quads = expand_prime_sets(triples, primes, sieve, req_remainder)
-    for quad in quads:
-        print(quad, sum(quad))
-generate_sets_with_remainder(1, primes, sieve)
-generate_sets_with_remainder(2, primes, sieve)
+            res.append((i,))
+    for i in range(size-1):
+        res = expand_prime_sets(res, primes, sieve, req_remainder)
+    return res
+
+set_size = 5 
+PRIME_MAX=10000
+sieve = Euler.sieveOfErasthenes(100000000)
+primes=[]
+for i in range(3,PRIME_MAX):
+    if sieve[i] == 1:
+        primes.append(i)
+primes.remove(5)
+print("done sieve")
+
+start =time.perf_counter()
+
+sets = generate_sets_with_remainder(set_size, 1, primes, sieve)
+sets = sets + generate_sets_with_remainder(set_size, 2, primes, sieve)
+min_sum = set_size * PRIME_MAX
+for i in sets:
+    min_sum = min(min_sum, sum(i))
+print(min_sum)
+
 end= time.perf_counter()
 print(end - start)
