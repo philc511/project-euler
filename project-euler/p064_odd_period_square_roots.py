fda50b7ca272374f8ffc037c758a861e0a0c569e
philc511 <phil.capon@fastmail.co.uk>
2020-11-10 21:27:31 +0000

    need to switch algorithm

--- project-euler/p064_odd_period_square_roots.py
+++ project-euler/p064_odd_period_square_roots.py
@@ -1,26 +1,30 @@
+#Use wkipedia: https://en.wikipedia.org/wiki/Periodic_continued_fraction#Canonical_form_and_repetend
+# or here https://web.archive.org/web/20151221205104/http://web.math.princeton.edu/mathlab/jr02fall/Periodicity/mariusjp.pdf
+
 import math
 import decimal
 
-def check_period(seq):
-    is_period=False
-    if len(seq) > 0 and len(seq) % 2 == 0:
-        is_period = seq[len(seq)//2:]== seq[:len(seq)//2] 
-    return is_period
+def check_period(a0, seq):
+    return len(seq) > 0 and seq[-1] == 2*a0
 
 c=0
 
-for i in range(1,10000):
+for i in range(1,100):
     a=decimal.Decimal(i).sqrt()
-    a=a-int(a)
-    if a > 0:
-        seq=[]
-        while not check_period(seq):
+    a0=int(a)
+    a=a-a0
+    print(a0,a)
+    if a0*a0 != i:
+        period=0
+        length=0
+        while period != 2*a0:
             a = decimal.Decimal(1)/a
             aint=int(a)
             a = a-aint
-            seq.append(aint)
-        if len(seq)//2 % 2 == 1:
+            period = aint
+            length += 1
+        if length % 2 == 1:
             c=c+1
-        print(i, len(seq)//2)
+        #print(i, length)
 print(c)
 
