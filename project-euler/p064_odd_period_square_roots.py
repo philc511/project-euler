689a51f414117ddd4672425b0be3546978a53dd0
philc511 <phil.capon@fastmail.co.uk>
2021-01-17 18:51:33 +0000

    latest changes

--- project-euler/p064_odd_period_square_roots.py
+++ project-euler/p064_odd_period_square_roots.py
@@ -1,30 +1,29 @@
-#Use wkipedia: https://en.wikipedia.org/wiki/Periodic_continued_fraction#Canonical_form_and_repetend
+# Just implement the alogorithm implemented here:
+# Use wkipedia: https://en.wikipedia.org/wiki/Periodic_continued_fraction#Canonical_form_and_repetend 
 # or here https://web.archive.org/web/20151221205104/http://web.math.princeton.edu/mathlab/jr02fall/Periodicity/mariusjp.pdf
-
+# Tried to figure out a way using decimal multiplication but that had too many rounding errors in, even with "decimal" package
 import math
-import decimal
 
 def check_period(a0, seq):
     return len(seq) > 0 and seq[-1] == 2*a0
 
 c=0
 
-for i in range(1,100):
-    a=decimal.Decimal(i).sqrt()
-    a0=int(a)
-    a=a-a0
-    print(a0,a)
-    if a0*a0 != i:
+for i in range(1,10000):
+    a=int(math.sqrt(i))
+    a0=a
+    m = 0
+    d = 1
+    if a*a != i:
         period=0
         length=0
         while period != 2*a0:
-            a = decimal.Decimal(1)/a
-            aint=int(a)
-            a = a-aint
-            period = aint
+            m = d*a - m
+            d = (i - m*m)/d
+            a = int((a0+m)/d)
+            period = a
             length += 1
         if length % 2 == 1:
             c=c+1
-        #print(i, length)
 print(c)
 
